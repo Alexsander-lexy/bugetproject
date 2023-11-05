c544c93af56bce239e29a15f8de4e34162c2a1ed
Roni <ronialvesart@gmail.com>
2023-11-05 16:04:47 -0300

    Atualizações com javacript, css e form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     const btnPrint = document.querySelector(".btn-print");
+    const btnRelatorio = document.querySelector(".btn-relatorio");
+    var descricao = document.querySelector(".descricao");
+    var termos = document.querySelector(".termos");
 
-    btnPrint.addEventListener("click",()=> window.print());
+    btnPrint.addEventListener("click",function(){
+        descricao.style.display ='none';
+        termos.style.display='block;'
+        window.print();
+        descricao.style.display ='block';
+    }
+   );
 
+    btnRelatorio.addEventListener("click",function(){
+        descricao.style.display ='block';
+        termos.style.display='none';
+        window.print();
+        termos.style.display ='block';
+        }
+    );
 });
 
